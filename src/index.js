0ff76ddbc60d496961f6b9fc5d06a333aaf9d44f
Arshad-Ali-1a <21bd1a661acsma@gmail.com>
2024-04-28 02:41:14 +0530

    implemented step-10

--- src/index.js
+++ src/index.js
@@ -1,12 +1,267 @@
 const readCSV = require("./csvReader");
 const { parseQuery } = require("./queryParser");
 
+const leftJoin = (data, joinTableData, parsed_query) => {
+  const JoinedData = [];
+
+  data.forEach((row) => {
+    let leftTableRowAddedAtLeastOnce = false;
+    joinTableData.forEach((joinRow) => {
+      if (
+        row[parsed_query.joinCondition.left] ===
+        joinRow[parsed_query.joinCondition.right]
+      ) {
+        JoinedData.push({ ...row, ...joinRow });
+        leftTableRowAddedAtLeastOnce = true;
+      }
+    });
+    if (!leftTableRowAddedAtLeastOnce) {
+      const rightObjectWithNullValues = Object.fromEntries(
+        Object.entries(joinTableData[0]).map(([key, value]) => [key, null])
+      );
+      JoinedData.push({ ...row, ...rightObjectWithNullValues });
+    }
+  });
+
+  return JoinedData;
+};
+
+const innerJoin = (data, joinTableData, parsed_query) => {
+  const JoinedData = [];
+
+  data.forEach((row) => {
+    joinTableData.forEach((joinRow) => {
+      if (
+        row[parsed_query.joinCondition.left] ===
+        joinRow[parsed_query.joinCondition.right]
+      ) {
+        JoinedData.push({ ...row, ...joinRow });
+      }
+    });
+  });
+  return JoinedData;
+};
+
+const rightJoin = (data, joinTableData, parsed_query) => {
+  const JoinedData = [];
+
+  joinTableData.forEach((joinRow) => {
+    let rightTableRowAddedAtLeastOnce = false;
+    data.forEach((row) => {
+      if (
+        row[parsed_query.joinCondition.left] ===
+        joinRow[parsed_query.joinCondition.right]
+      ) {
+        JoinedData.push({ ...row, ...joinRow });
+        rightTableRowAddedAtLeastOnce = true;
+      }
+    });
+    if (!rightTableRowAddedAtLeastOnce) {
+      const leftObjectWithNullValues = Object.fromEntries(
+        Object.entries(data[0]).map(([key, value]) => [key, null])
+      );
+      JoinedData.push({ ...joinRow, ...leftObjectWithNullValues });
+    }
+  });
+
+  return JoinedData;
+};
+
+const handleWhereClauses = (data, parsed_query) => {
+  const filtered_data = [];
+
+  data.forEach((row) => {
+    let allWhereClausesMatch = true; //!this is for AND, make for OR also
+    if (parsed_query.whereClauses.length) {
+      parsed_query.whereClauses.forEach(({ field, operator, value }) => {
+        //prettier-ignore
+        if (!eval(`"${row[field]}" ${operator === "=" ? "==" : operator} "${value}"`)){ //!added "row[field]==null" for ignoring where when field is null, which happensS in case of joins
+
+          if(parsed_query.joinTable && field.split(".")[0]==parsed_query.table && row[field]==null){
+            //if tables are joined AND field is from the main table AND field is null() because it was from the join.. then allow it in the result
+            //do nothing
+          }
+          else
+            allWhereClausesMatch = false;
+        }
+      });
+    }
+
+    if (!allWhereClausesMatch) {
+      return;
+    }
+
+    filtered_data.push(row);
+  });
+
+  return filtered_data;
+};
+
+const rowWithAggregates = (
+  groupedDataObj,
+  row,
+  aggregateFunctions,
+  groupFieldData,
+  isNewGroup
+) => {
+  if (isNewGroup) {
+    aggregateFunctions.forEach((aggregateFunction) => {
+      //identify the aggregate function
+      const aggregateFunctionName = aggregateFunction.split("(")[0]; //TODO: might need to handle lowercase aggregate functions
+      const aggregateFunctionField = aggregateFunction
+        .split("(")[1]
+        .split(")")[0];
+
+      switch (aggregateFunctionName) {
+        case "COUNT":
+          row[aggregateFunction] = 1;
+          break;
+
+        case "AVG":
+          row[aggregateFunction] = parseInt(row[aggregateFunctionField]);
+          row[`_SUM(${aggregateFunctionField})`] = parseInt(
+            row[aggregateFunctionField]
+          ); //for calculating average later
+          break;
+
+        default:
+          row[aggregateFunction] = parseInt(row[aggregateFunctionField]); //MAX,MIN,SUM
+          break;
+      }
+    });
+    row["_count"] = 1;
+    return row;
+  } else {
+    //for existing group
+    const groupRow = groupedDataObj[groupFieldData];
+
+    aggregateFunctions.forEach((aggregateFunction) => {
+      //identify the aggregate function
+      const aggregateFunctionName = aggregateFunction.split("(")[0]; //TODO: might need to handle lowercase aggregate functions
+      const aggregateFunctionField = aggregateFunction
+        .split("(")[1]
+        .split(")")[0];
+
+      switch (aggregateFunctionName) {
+        case "COUNT":
+          groupRow[aggregateFunction] += 1; //! in case of no null values, the answer for count(x) and count(*) will be same.. so this works.
+          break;
+
+        case "SUM":
+          groupRow[aggregateFunction] += parseInt(row[aggregateFunctionField]);
+          break;
+
+        case "AVG":
+          groupRow[`_SUM(${aggregateFunctionField})`] += parseInt(
+            row[aggregateFunctionField]
+          );
+
+          groupRow[aggregateFunction] =
+            groupRow[`_SUM(${aggregateFunctionField})`] /
+            (groupRow["_count"] + 1); //groupRow["_count"]+1 as the count is increased after the switch case
+
+          break;
+
+        case "MAX":
+          groupRow[aggregateFunction] = Math.max(
+            groupRow[aggregateFunction],
+            parseInt(row[aggregateFunctionField])
+          );
+          break;
+
+        case "MIN":
+          groupRow[aggregateFunction] = Math.min(
+            groupRow[aggregateFunction],
+            parseInt(row[aggregateFunctionField])
+          );
+          break;
+
+        default:
+          throw new Error(`Invalid aggregate function: ${aggregateFunction}`);
+      }
+      groupRow["_count"] += 1;
+    });
+
+    return groupRow;
+  }
+};
+
+const applyGroupBy = (data, parsed_query) => {
+  if (parsed_query.groupByFields == null) {
+    return data;
+  }
+
+  //identifying aggregate functions
+  const aggregateFunctions = [...parsed_query.fields].filter(
+    (field) => field.includes("(") && field.includes(")")
+  );
+
+  const groupedDataObj = {};
+  let groupField = parsed_query.groupByFields[0]; //!assuming only one group by field for now, change this
+
+  data.forEach((row) => {
+    const groupFieldData = row[groupField];
+    if (groupFieldData in groupedDataObj) {
+      groupedDataObj[groupFieldData] = rowWithAggregates(
+        groupedDataObj,
+        row,
+        aggregateFunctions,
+        groupFieldData,
+        false
+      );
+    } else {
+      groupedDataObj[groupFieldData] = rowWithAggregates(
+        groupedDataObj,
+        row,
+        aggregateFunctions,
+        groupFieldData,
+        true
+      );
+    }
+    // console.log(groupedDataObj);
+  });
+
+  return Object.values(groupedDataObj);
+};
+
+const applyAggrateWithoutGroupBy = (data, parsed_query) => {
+  const aggregateFunctions = [...parsed_query.fields].filter(
+    (field) => field.includes("(") && field.includes(")")
+  );
+
+  const groupedDataObj = {};
+  const keyToReplaceGroupFieldData = null;
+
+  data.forEach((row) => {
+    if (keyToReplaceGroupFieldData in groupedDataObj) {
+      groupedDataObj[keyToReplaceGroupFieldData] = rowWithAggregates(
+        groupedDataObj,
+        row,
+        aggregateFunctions,
+        keyToReplaceGroupFieldData,
+        false
+      );
+    } else {
+      groupedDataObj[keyToReplaceGroupFieldData] = rowWithAggregates(
+        groupedDataObj,
+        row,
+        aggregateFunctions,
+        keyToReplaceGroupFieldData,
+        true
+      );
+    }
+    // console.log(groupedDataObj);
+  });
+
+  return Object.values(groupedDataObj);
+};
+
 const executeSELECTQuery = async (query) => {
   const parsed_query = parseQuery(query);
+  // console.log("query:", parsed_query);
   let data = await readCSV(`./${parsed_query.table}.csv`);
 
   if (parsed_query.joinTable) {
-    const JoinedData = [];
     let joinTableData = await readCSV(`./${parsed_query.joinTable}.csv`);
 
     data = data.map((row) => {
@@ -26,46 +281,34 @@ const executeSELECTQuery = async (query) => {
     });
 
     //lll
-    data.forEach((row) => {
-      let leftTableRowAddedAtLeastOnce = false;
-      joinTableData.forEach((joinRow) => {
-        if (
-          row[parsed_query.joinCondition.left] ===
-          joinRow[parsed_query.joinCondition.right]
-        ) {
-          JoinedData.push({ ...row, ...joinRow });
-          leftTableRowAddedAtLeastOnce = true;
-        }
-      });
-      if (!leftTableRowAddedAtLeastOnce && parsed_query.joinType === "LEFT") {
-        const rightObjectWithNullValues = Object.fromEntries(
-          Object.entries(joinTableData[0]).map(([key, value]) => [key, null])
-        );
-        JoinedData.push({ ...row, ...rightObjectWithNullValues });
-      }
-    });
 
-    data = JoinedData;
+    switch (parsed_query.joinType) {
+      case "LEFT":
+        data = leftJoin(data, joinTableData, parsed_query);
+        break;
+
+      case "RIGHT":
+        data = rightJoin(data, joinTableData, parsed_query);
+        break;
+
+      default:
+        data = innerJoin(data, joinTableData, parsed_query);
+        break;
+    }
   }
 
   // console.log(data);
+  data = handleWhereClauses(data, parsed_query);
+
+  if (parsed_query.groupByFields != null)
+    data = applyGroupBy(data, parsed_query);
+  //!assuming only one group by field for now, change this
+  else if (parsed_query.hasAggregateWithoutGroupBy)
+    data = applyAggrateWithoutGroupBy(data, parsed_query);
 
   const result = [];
 
-  await data.forEach((row) => {
-    let allWhereClausesMatch = true;
-    if (parsed_query.whereClauses.length) {
-      parsed_query.whereClauses.forEach(({ field, operator, value }) => {
-        //prettier-ignore
-        if (!eval(`"${row[field]}" ${operator === "=" ? "==" : operator} "${value}"`))
-          allWhereClausesMatch = false;
-      });
-    }
-
-    if (!allWhereClausesMatch) {
-      return;
-    }
-
+  data.forEach((row) => {
     const obj = {};
     parsed_query.fields.forEach((field) => {
       obj[field] = row[field];
@@ -78,3 +321,9 @@ const executeSELECTQuery = async (query) => {
 };
 
 module.exports = executeSELECTQuery;
+
+// (async () => {
+//   console.log(
+//     await executeSELECTQuery("SELECT AVG(age) FROM student WHERE age > 22")
+//   );
+// })();
--- src/queryParser.js
+++ src/queryParser.js
@@ -1,8 +1,49 @@
 const { parse } = require("json2csv");
 
+const parseJoinClause = (query) => {
+  const re =
+    /( (?<join_type>\w+) JOIN (?<join_table>\w+) ON (?<join_left>(\w|\.)+)\s?=\s?(?<join_right>(\w|\.)+))/;
+  const matches = query.match(re);
+  if (!matches) {
+    return { joinCondition: null, joinTable: null, joinType: null };
+  }
+
+  return {
+    joinCondition: {
+      left: matches.groups.join_left,
+      right: matches.groups.join_right,
+    },
+    joinTable: matches.groups.join_table,
+    joinType: matches.groups.join_type,
+  };
+};
+
+const parseGroupBy = (query) => {
+  const re = /(GROUP BY (?<group_fields>(\w|\.|(,\s?))+))/;
+
+  const matches = query.match(re);
+
+  if (!matches) {
+    return { groupByFields: null };
+  }
+
+  return {
+    groupByFields: matches.groups.group_fields
+      .replaceAll(/\s+/g, "")
+      .split(","),
+  };
+};
+
+const hasAggregateFunction = (fieldsString) => {
+  const re = /(COUNT|SUM|AVG|MIN|MAX)\((\w|\.)+|\*\)/;
+  return fieldsString.match(re) !== null;
+};
+
 const parseQuery = (query) => {
   const re =
-    /^SELECT (?<fields>(\w|\.)+(,\s?(\w|\.)+)*) FROM (?<table>\w+)( (?<join_type>\w+) JOIN (?<join_table>\w+) ON (?<join_left>(\w|\.)+)\s?=\s?(?<join_right>(\w|\.)+))?( WHERE (?<where>(\w|\.|[=><!]|\s)+))?$/;
+    /SELECT (?<fields>(\w|\.|\(|\)|\*)+(,\s?(\w|\.|\(|\)|\*)+)*) FROM (?<table>\w+)( (?<join_type>\w+) JOIN (?<join_table>\w+) ON (?<join_left>(\w|\.)+)\s?=\s?(?<join_right>(\w|\.)+))?( WHERE (?<where>(\w|\.|[=><!]|\s|'|")+?)(?=\s*GROUP BY|\s*$))?/;
+
+  //! removed full match from regex
   const re_where_operator = /([=><!]+)/;
 
   const matches = query.match(re);
@@ -24,25 +65,36 @@ const parseQuery = (query) => {
         throw new Error("Invalid where clause");
       }
       const [field, operator, value] = clause.split(re_where_operator);
+      // console.log(clause.split(re_where_operator));
 
       whereClauses.push({
         field: field.trim(),
         operator: operator.trim(),
-        value: value.trim(),
+        value: value.replaceAll(/'|"/g, "").trim(),
       });
     });
   }
+
+  let groupByFieldsObject = parseGroupBy(query);
 
   return {
     fields,
     table: matches.groups.table,
     whereClauses: whereClauses,
+    groupByFields: groupByFieldsObject.groupByFields,
     joinCondition: matches.groups.join_table
       ? { left: matches.groups.join_left, right: matches.groups.join_right }
       : null,
     joinTable: matches.groups.join_table ?? null,
     joinType: matches.groups.join_type ?? null,
+    hasAggregateWithoutGroupBy:
+      hasAggregateFunction(matches.groups.fields) &&
+      !(groupByFieldsObject.groupByFields !== null),
   };
 };
 
-module.exports = { parseQuery };
+module.exports = { parseQuery, parseJoinClause };
+
+// console.log(
+//   parseQuery(`SELECT age, COUNT(*) FROM student WHERE age > 22 GROUP BY age`)
+// );
